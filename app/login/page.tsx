bb7c474b8f8600519c5f22e932f6dac961979253
Sterling7718 <nora.sorence@gmail.com>
2024-04-04 17:41:46 +0800

    change to flexbox

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,30 +11,14 @@ export default async function Page() {
           <div className={classes.textBlock2}>Clearance</div>
         </div>
         <div className={classes.loginFrame}>
-          <button className={classes.buttonMSOA}>
-            <div className={classes.buttonContent}>
-              <div className={classes.image3} />
-              <div className={classes.office365Login}>Office 365 Login</div>
+          <div className={classes.loginWithOffice365}>
+            <div className={classes.textBlock5}>Login</div>
+            <div className={classes.textBlock6}>
+              <p className={classes.labelWrapper}>
+                <span className={classes.label}>with </span>
+                <span className={classes.label2}>Office 365</span>
+              </p>
             </div>
-          </button>
-
-          <div className={classes.or}>or</div>
-
-          <button className={classes.buttonLogin}>
-            <div className={classes.loginWrapper}>
-              <div className={classes.lOGIN}>LOGIN</div>
-            </div>
-          </button>
-
-          <div className={classes.rememberMeCheckBox}>
-            <input
-              type="checkbox"
-              id="rememberMeCheckbox"
-              className={classes.checkboxInput}
-            />
-            <label htmlFor="rememberMeCheckbox" className={classes.rememberMe}>
-              Remember me
-            </label>
           </div>
 
           <div className={classes.input}>
@@ -61,16 +45,36 @@ export default async function Page() {
                 className={classes.inputField}
               />
             </div>
-          </div>
-          <div className={classes.loginWithOffice365}>
-            <div className={classes.textBlock5}>Login</div>
-            <div className={classes.textBlock6}>
-              <p className={classes.labelWrapper}>
-                <span className={classes.label}>with </span>
-                <span className={classes.label2}>Office 365</span>
-              </p>
+
+            <div className={classes.rememberMeCheckBox}>
+              <input
+                type="checkbox"
+                id="rememberMeCheckbox"
+                className={classes.checkboxInput}
+              />
+              <label
+                htmlFor="rememberMeCheckbox"
+                className={classes.rememberMe}
+              >
+                Remember me
+              </label>
             </div>
           </div>
+
+          <button className={classes.buttonMSOA}>
+            <div className={classes.buttonContent}>
+              <div className={classes.image3} />
+              <div className={classes.office365Login}>Office 365 Login</div>
+            </div>
+          </button>
+
+          <div className={classes.or}>or</div>
+
+          <button className={classes.buttonLogin}>
+            <div className={classes.loginWrapper}>
+              <div className={classes.lOGIN}>LOGIN</div>
+            </div>
+          </button>
         </div>
       </div>
       <div className={classes.stiLogo}></div>
