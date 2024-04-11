84f1a8941ba2c42409e69602589c17b2157de6b4
Sterling7718 <STIStudentClearanceSystem@outlook.com>
2024-04-11 18:24:57 +0800

    try to fix page.tsx

--- app/users/admin/page.tsx
+++ app/users/admin/page.tsx
@@ -1,14 +1,34 @@
-import { Metadata } from 'next';
-import SideNav from '@/app/components/SideNav'; // adjust the import path as per your project structure
+'use client';
+// import { Metadata } from 'next';
+import SideNav from '@/app/components/SideNav';
+import { useRouter } from 'next/navigation'; // Import useRouter from next/navigation
 
-export const metadata: Metadata = {
-  title: 'Admin',
-};
+// export const metadata: Metadata = {
+//   title: 'Admin',
+// };
+
+interface Button {
+  label: string;
+  onClick: () => void;
+}
 
 export default function Page() {
+  const router = useRouter();
+
+  function redirectToAdminPage() {
+    router.push('/users/admin');
+  }
+
+  const assignTaskBtns: Button[] = [
+    {
+      label: 'Role Setter',
+      onClick: redirectToAdminPage,
+    },
+  ];
+
   return (
     <div className="flex h-screen bg-blue-300">
-      <SideNav title={'Admin'} assignTaskBtn="Role Setter" />
+      <SideNav title={'Admin'} assignTaskBtns={assignTaskBtns} />
       <div className="flex-grow p-8">
         <header>
           <h1 className="font-arimo ml-16 text-4xl font-bold">Role Setter</h1>
