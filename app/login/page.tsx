021f45f4cb9b5ca7203709a93d4a30f1c9ef78cc
Sterling7718 <STIStudentClearanceSystem@outlook.com>
2024-04-17 03:37:44 +0800

    Update Login page and styles; adjust for smaller screens
    The commit includes changes to the Login page and its associated styles. It adds a media query for smaller screens, adjusts the layout and sizes of various elements, and updates the text to fit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -17,12 +17,12 @@ export default function LoginPage() {
 
         <div className={classes.loginFrame}>
           <div className={classes.loginWithOffice365}>
-            <div className={classes.textBlock5}>Login</div>
-            <div className={classes.textBlock6}>
-              <p className={classes.labelWrapper}>
-                <span className={classes.label}>with </span>
-                <span className={classes.label2}>Office 365</span>
-              </p>
+            <div className={classes.textBlock5}>
+              Login
+              <div className={classes.textBlock6}>
+                with
+                <span className={classes.office365}> Office 365</span>
+              </div>
             </div>
           </div>
           <div className="flex justify-center">
