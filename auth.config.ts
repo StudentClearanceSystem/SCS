1bc6f910ced5a091d5209e5e9ee5df8e30385ac7
StudentClearanceSystem <166558750+StudentClearanceSystem@users.noreply.github.com>
2024-04-17 06:55:32 +0800

    "Removed hardcoded '/users/admin' from authConfig and updated header margins in user dashboard pages"
    This commit includes changes to the positioning of header elements in various user dashboard pages and updates to the authentication configuration

--- auth.config.ts
+++ auth.config.ts
@@ -7,13 +7,13 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/users/admin');
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
 
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/users/admin', nextUrl));
+        return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
     },
