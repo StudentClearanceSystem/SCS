172b58e791e9058f1e817e8199e7091c5e746d96
StudentClearanceSystem <166558750+StudentClearanceSystem@users.noreply.github.com>
2024-06-13 04:14:29 +0800

    "Removed DropdownWithInput component, registrarRemarks prop, and related logic from ActionCell component"

--- app/users/registrar/Modify_Student/ActionCell.tsx
+++ app/users/registrar/Modify_Student/ActionCell.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { RadioGroup, Radio } from '@nextui-org/radio';
 
-import DropdownWithInput from '@/app/components/DropdownWithInput';
 import {
   StudentDetailsRegistrar,
   updateRegistrarRemarks,
@@ -11,7 +10,6 @@ import {
 const ActionCell = ({
   isRegistrarCleared,
   studentNo,
-  registrarRemarks,
   studentDetails, // Add studentDetails prop
 }: {
   isRegistrarCleared: boolean;
@@ -23,56 +21,14 @@ const ActionCell = ({
     isRegistrarCleared ? 'Cleared' : 'Uncleared',
   );
 
-  const handleRadioChange = (value: string) => {
-    setSelectedValue(value);
-    const updatedDetails = {
-      ...studentDetails,
-      is_registrar_cleared: value === 'Cleared',
-    };
-    updateRegistrarStatus(updatedDetails);
-  };
-
-  const handleRemarksSubmit = (remarks: string) => {
-    const updatedDetails = {
-      ...studentDetails,
-      cashier_remarks: remarks,
-    };
-    updateRegistrarRemarks(updatedDetails);
-  };
-
-  const getBackgroundColor = () => {
-    return selectedValue === 'Cleared' ? 'bg-green-400' : 'bg-red-300';
-  };
-
   useEffect(() => {
     setSelectedValue(isRegistrarCleared ? 'Cleared' : 'Uncleared');
   }, [isRegistrarCleared]);
 
   return (
     <div
-      className={`flex min-w-[300px] max-w-[350px] items-center justify-center ${getBackgroundColor()}`}
-    >
-      <RadioGroup
-        value={selectedValue}
-        orientation="horizontal"
-        onValueChange={handleRadioChange}
-      >
-        <Radio value="Cleared" color="success">
-          Cleared
-        </Radio>
-        <Radio value="Uncleared" color="danger">
-          Uncleared
-        </Radio>
-      </RadioGroup>
-      <DropdownWithInput
-        disabled={selectedValue === 'Cleared'}
-        initialInputValue={registrarRemarks}
-        placeholder="Remarks..."
-        buttonLabel="Submit"
-        bgColor={'#FF7EFF'} // Pass the bgColor prop
-        onSubmit={handleRemarksSubmit}
-      />
-    </div>
+      className={'lex min-w-[300px] max-w-[350px] items-center justify-center'}
+    ></div>
   );
 };
 
